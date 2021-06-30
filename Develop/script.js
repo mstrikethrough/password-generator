1fc6bab4612ad63648da10fe7e43b54691a92733
mstrikethrough <avila_024@yahoo.com>
2021-06-30 16:32:58 -0500

    made button prompt questions

--- Develop/script.js
+++ Develop/script.js
@@ -5,12 +5,6 @@ var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var special = ['!', '"', '#', '$', '%', '^', '&', '*', '(', ')', '~', '[', ']', ';', ':', '/', '|', '?', '<', '>', '.', ',', '-', '_', '=', '+'];
-// var passwordLength;
-// var useLower;
-// var useUpper;
-// var useNumber;
-// var useSpecial;
-
 
 //NEED TO TIE THESE TO 'GENERATE PASSWORD' BUTTON
 // qualifying criteria usage questions
