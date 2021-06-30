cc558a12adf3764ef92faf09deb6e3aa4783fdb0
mstrikethrough <avila_024@yahoo.com>
2021-06-30 16:31:42 -0500

    made criteria alerts

--- Develop/script.js
+++ Develop/script.js
@@ -1,67 +1,78 @@
 // Assignment code here
 
-function questions() {
+// variable declarations
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var special = ['!', '"', '#', '$', '%', '^', '&', '*', '(', ')', '~', '[', ']', ';', ':', '/', '|', '?', '<', '>', '.', ',', '-', '_', '=', '+'];
+// var passwordLength;
+// var useLower;
+// var useUpper;
+// var useNumber;
+// var useSpecial;
 
-  var characterCount = window.prompt("Choose between 8-128 characters for your password length");
-  while (characterCount < 8 || characterCount > 128 || isNaN(characterCount)) {
-    window.alert("Must select a value between 8 and 128")
-    return questions();
-    //var characterCount = parseInt(characterCount);//
-    console.log(characterCount);
+
+//NEED TO TIE THESE TO 'GENERATE PASSWORD' BUTTON
+// qualifying criteria usage questions
+function length() {
+  var passwordLength = prompt("Choose between 8-128 characters for your password length");
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Must select a value between 8 and 128");
+    return length();
   }
+};
   
-  var lowerCase = window.confirm("Click OK to include lowercase letters");
+function getLowerCase() {
+  var useLower = prompt('Use lower case letters? Type "yes" or "no".');
+  if (useLower === 'y' || useLower === 'yes'  || useLower === 'Yes') {
+    return true;
+  } else if (useLower === 'n' || useLower === 'no' || useLower === 'No') {
+    return false;
+  } else (useLower === null || useLower === '')
+  alert('Please type "yes" or "no".');
+  getLowerCase();
+};
 
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  console.log(lowerCase);  
-  
-  
-  var upperCase = window.confirm("Click OK to include uppercase letters");
+function getUpperCase() {
+  var useUpper = prompt('Use upper case letters? Type "yes" or "no".');
+  if (useUpper === 'y' || useUpper === 'yes'  || useUpper === 'Yes') {
+    return true;
+  } else if (useUpper === 'n' || useUpper === 'no' || useUpper === 'No') {
+    return false;
+  } else (useUpper === null || useUpper === '')
+  alert('Please type "yes" or "no".');
+  getUpperCase();
+};
 
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-  console.log(upperCase);
- 
-  
-  var numeric = window.confirm("Click OK to include numbers");
+function getNumber() {
+  var useNumber = prompt('Use numbers? Type "yes" or "no".');
+  if (useNumber === 'y' || useNumber === 'yes'  || useNumber === 'Yes') {
+    return true;
+  } else if (useNumber === 'n' || useNumber === 'no' || useNumber === 'No') {
+    return false;
+  } else (useNumber === null || useNumber === '')
+  alert('Please type "yes" or "no".');
+  getNumber();
+};
 
-  var numeric = "0123456789";  
-  console.log(numeric);
+function getSpecial() {
+  var useSpecial = prompt('Use special characters? Type "yes" or "no".');
+  if (useSpecial === 'y' || useSpecial === 'yes'  || useSpecial === 'Yes') {
+    return true;
+  } else if (useSpecial === 'n' || useSpecial === 'no' || useSpecial === 'No') {
+    return false;
+  } else (useSpecial === null || useSpecial === '')
+  alert('Please type "yes" or "no".');
+  getSpecial();
+};
 
-  
-  var specialChars = window.confirm("Click OK to include special characters");
-  var specialChars = "`~!@#$%^&*()-_=+[}{]";  
-  console.log(specialChars);
-
-} 
-
-function getLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
- }
-console.log(getLower());
-
-function getUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
- }
-console.log(getUpper());
-
-function getNumeric() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
- }
-console.log(getNumeric());
-
-function getSpecialChars() {
-  const specialCharList = "~`!@#$%^&*()_-+={[}];:<>|";
-  return specialCharList[Math.floor(Math.random() * specialCharList.length)];
- }
-console.log(getSpecialChars());
-
-
-
-
-
-
-
-
+function generatePassword() {
+  length();
+  getLowerCase();
+  getUpperCase();
+  getNumber();
+  getSpecial();
+};
 
 
 // Get references to the #generate element
@@ -73,8 +84,12 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
